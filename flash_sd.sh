8b5b92235e6559d4c6c328665f792218c4775e7b
@lex <alex.mobigo@gmail.com>
2017-03-28 08:17:59 -0300

    no owner in FAT

--- flash_sd.sh
+++ flash_sd.sh
@@ -57,7 +57,7 @@ rm -fR erootfs
 set +e
 mkdir eboot
 mount $SDCARD"1" eboot
-tar -xvpzf boot_nanopia64_rc1.tar.gz -C ./eboot --numeric-ow
+tar -xvpzf boot_nanopia64_rc1.tar.gz -C ./eboot
 sync
 umount eboot
 rm -fR eboot
